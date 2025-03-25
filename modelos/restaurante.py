620e393047d4deec59b5c28c8298bc2625858da4
nikarcosta <mrc32467098@outlook.com>
2025-03-25 07:16:37 -0300

    feat: add methods adicionar_bebida_no_cardapio & adicionar_prato_no_cardapio

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -8,6 +8,7 @@ class Restaurante:
         self._categoria = categoria
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -41,3 +42,9 @@ class Restaurante:
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
 
+
+    def adicionar_bebida_no_cardapio(self, bebida):
+        self._cardapio.append(bebida)
+
+    def adicionar_prato_no_cardapio(self, prato):
+        self._cardapio.append(prato)
