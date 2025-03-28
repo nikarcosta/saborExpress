400a6eb10d66f12021270aea034601fbfcb0e1aa
nikarcosta <mrc32467098@outlook.com>
2025-03-28 07:44:49 -0300

    feat: add abstract method aplicar_desconto

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante1 = Restaurante('Restaurante da Vovó', 'Comida Caseira')
 bebida = Bebida('cafézinho', 5.00, 'pequeno')
 prato = Prato('Pão de Queijo', 6.00, 'Pão de queijo caseiro')
+bebida.aplicar_desconto()
+prato.aplicar_desconto()
 restaurante1.adicionar_no_cardapio(bebida)
 restaurante1.adicionar_no_cardapio(prato)
 
