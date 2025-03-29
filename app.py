a12d9cdba902b826fbf577997f70d5a239a98099
nikarcosta <mrc32467098@outlook.com>
2025-03-29 10:01:05 -0300

    feat: add class Sobremesa

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from modelos.cardapio.sobremesa import Sobremesa
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
@@ -5,10 +6,13 @@ from modelos.cardapio.prato import Prato
 restaurante1 = Restaurante('Restaurante da Vovó', 'Comida Caseira')
 bebida = Bebida('cafézinho', 5.00, 'pequeno')
 prato = Prato('Pão de Queijo', 6.00, 'Pão de queijo caseiro')
+sobremesa = Sobremesa('Sunday', 10.00, 'bebida', 'grande', 'Sunday delicioso de chocolate')
 bebida.aplicar_desconto()
 prato.aplicar_desconto()
+sobremesa.aplicar_desconto()
 restaurante1.adicionar_no_cardapio(bebida)
 restaurante1.adicionar_no_cardapio(prato)
+restaurante1.adicionar_no_cardapio(sobremesa)
 
 def main():
     restaurante1.exibir_cardapio()
